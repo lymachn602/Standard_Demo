841c75947732f75c29c2deab222f6114aab00f2f
lymachn602 <287643905@qq.com>
2017-05-22 20:38:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linyuming/standard_demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.linyuming.standard_demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+   private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("MainActivity",this.toString());
+        setContentView(R.layout.activity_main);
+        btn  = (Button) findViewById(R.id.button1);
+        btn .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
